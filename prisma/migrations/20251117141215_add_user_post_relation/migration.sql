598e1f943194de18863fe0b48a3c281f1c61245e
Stefania Sgreva <108456339+StefaniaSgreva@users.noreply.github.com>
2025-11-17 15:15:14 +0100

    feat: add one-to-many relation between User and Post in prisma.schema

---
+++ prisma/migrations/20251117141215_add_user_post_relation/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE `post` ADD COLUMN `authorId` INTEGER NULL;
+
+-- AddForeignKey
+ALTER TABLE `Post` ADD CONSTRAINT `Post_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
